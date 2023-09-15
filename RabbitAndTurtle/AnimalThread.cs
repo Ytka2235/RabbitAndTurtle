123f95ad8d8151c33a730502161c017daf73d456
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-15 10:41:49 +0300

    Добавьте файлы проекта.

---
+++ RabbitAndTurtle/AnimalThread.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+internal class AnimalThread
+{
+    public Thread thread;
+    public string name;
+
+    public AnimalThread(string name)
+    {
+        this.name = name;
+    }
+}
+
---
+++ RabbitAndTurtle/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+
+    public static int x = 0; public static int y = 0;
+    private static void Main(string[] args)
+    {
+        Thread Rabbit = new Thread(RunRabbit);
+        Thread Turtle = new Thread(RunTurtle);
+        Rabbit.Start();
+        Turtle.Start();
+        while (x == 100 || y == 100)
+        {
+            if (x > (y + 10))
+            {
+                Turtle.Priority = ThreadPriority.Normal;
+                Rabbit.Priority = ThreadPriority.Lowest;
+            }
+            if (y > (x + 10))
+            {
+                Turtle.Priority = ThreadPriority.Lowest;
+                Rabbit.Priority = ThreadPriority.Normal;
+            }
+        }
+        Console.ReadKey();
+
+    }
+
+    public static void RunRabbit()
+    {
+        for ( x = 0; x < 1001; x++)
+        {
+            Console.WriteLine("Rabbit пробежал {0} метров",  x);
+            
+        }
+    }
+
+    public static void RunTurtle()
+    {
+        for (y = 0; y < 1001; y++)
+        {
+            Console.WriteLine("Turtle пробежал {0} метров", y);
+            
+        }
+    }
+}
